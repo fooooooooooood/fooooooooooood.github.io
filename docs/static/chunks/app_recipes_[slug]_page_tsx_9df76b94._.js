1af738db6df1517689f395529aebe568ab947215
Food? <233880864+fooooooooooood@users.noreply.github.com>
2026-08-01 20:28:11 -0600

    npm run build

--- docs/static/chunks/app_recipes_[slug]_page_tsx_9df76b94._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_23713677._.js"
-],
-    source: "dynamic"
-});
