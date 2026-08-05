f5d602ff4506d31fe0580ab51fa1a91dd2dcde9f
Food? <233880864+fooooooooooood@users.noreply.github.com>
2026-08-04 21:13:22 -0600

    Add even more songs

--- docs/static/chunks/app_recipes_[slug]_page_tsx_89f5f50e._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_4a2b14d1._.js"
-],
-    source: "dynamic"
-});
